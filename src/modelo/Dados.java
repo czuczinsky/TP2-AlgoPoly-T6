c6761765b2a56cc1dec5d0c9644813b04d30f69e
hhuza <hhuza@192.168.1.103>
2017-11-10 14:18:30 -0300

    Dados y DadosTest

--- src/modelo/Dados.java
+++ src/modelo/Dados.java
@@ -1,5 +1,28 @@
 package modelo;
 
 public class Dados {
+	private int dado1;
+	private int dado2;
 
+	public Dados() {
+		tirar();
+	}
+
+	public void tirar() {
+		dado1 = 1 + (int)(Math.random() * 6);
+		dado2 = 1 + (int)(Math.random() * 6);
+	}
+
+	public int getSuma() {
+		return dado1 + dado2;
+	}
+	
+	public int getDado1() {
+		return dado1;
+	}
+	
+	public int getDado2() {
+		return dado2;
+	}
 }
+
---
+++ test/DadosTest.java
@@ -0,0 +1,21 @@
+package test;
+
+import static org.junit.Assert.*;
+import org.junit.Test;
+import modelo.*;
+
+public class DadosTest {
+
+	@Test
+	public void test01Tirar() {
+		Dados dados = new Dados();
+		for (int i = 0; i < 1000; i++) {
+			assertTrue(dados.getDado1() >= 1);
+			assertTrue(dados.getDado2() >= 1);
+			assertTrue(dados.getSuma() >= 2);
+			assertTrue(dados.getDado1() <= 6);
+			assertTrue(dados.getDado2() <= 6);
+			assertTrue(dados.getSuma() <= 12);
+		}
+	}
+}
