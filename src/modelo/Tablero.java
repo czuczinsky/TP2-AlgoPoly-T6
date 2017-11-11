6a546421dd3cce2984b5d5f068e8f879a7bed7d3
Santiago <czuczinsky@hotmail.com>
2017-11-11 18:30:10 -0300

    Tablero - Refactorizo avanzar, agrego retroceder y mas tests

--- src/modelo/Tablero.java
+++ src/modelo/Tablero.java
@@ -13,11 +13,26 @@ public class Tablero {
 
 	public void avanzar(Jugador jugador) {
 		Casillero posicionJugador = jugador.getPosicion();
-		ListIterator<Casillero> iterador = casilleros.listIterator(casilleros.indexOf(posicionJugador) + 1);
-		jugador.moverA(iterador.next());
+		ListIterator<Casillero> iterador = this.casilleros.listIterator(this.casilleros.indexOf(posicionJugador) + 1);
+		if (iterador.hasNext()) {
+			jugador.moverA(iterador.next());
+		} else {
+			jugador.moverA(this.casilleros.get(0));
+		}
 	}
 	
 	public void agregarCasillero(Casillero casillero) {
 		this.casilleros.add(casillero);
+	}
+
+	public void retroceder(Jugador jugador) {
+		Casillero posicionJugador = jugador.getPosicion();
+		ListIterator<Casillero> iterador = this.casilleros.listIterator(this.casilleros.indexOf(posicionJugador));
+		if (iterador.hasPrevious()) {
+			jugador.moverA(iterador.previous());
+		} 
+		else {
+			jugador.moverA(this.casilleros.get(this.casilleros.size()-1));
+		}
 	}
 }
