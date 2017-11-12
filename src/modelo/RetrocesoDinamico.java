7e147545052d02aa00073e9ce0cf57eaeb701ade
Santiago <czuczinsky@hotmail.com>
2017-11-11 21:44:46 -0300

    revert commit

--- src/modelo/RetrocesoDinamico.java
+++ src/modelo/RetrocesoDinamico.java
@@ -2,19 +2,10 @@ package modelo;
 
 public class RetrocesoDinamico implements Casillero {
 
-	
-	private Tablero tablero;
-
-	public RetrocesoDinamico(Tablero tablero) {
-		this.tablero = tablero;
-	}
-
 	@Override
 	public void ocupar(Jugador jugador) {
-		Dados dados = Dados.getDados();
-		for (int i = 0;i<dados.getSuma()-2;i++) {
-			this.tablero.retroceder(jugador);
-		}
+		// TODO Auto-generated method stub
+		
 	}
 
 }
