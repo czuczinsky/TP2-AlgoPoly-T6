219a60ef11ec10ef11d7ca8686a995d515f65040
acevedoanalia <analiaacevedo020@gmail.com>
2017-11-12 12:27:51 -0300

    Se agregan tests y refactoring de RetrocesoDinamico

--- src/modelo/RetrocesoDinamico.java
+++ src/modelo/RetrocesoDinamico.java
@@ -4,39 +4,55 @@ public class RetrocesoDinamico implements Casillero {
 
 	private Tablero tablero;
 
+	
 	public RetrocesoDinamico(Tablero tablero) {
 		this.tablero = tablero;
 	}
 	
-	@Override
-	public void ocupar(Jugador jugador) {
+	
+	public void retrocederConDadosEntre2y6(Jugador jugador) {
 		Dados dados=Dados.getDados();
 		
 		if((dados.getSuma()>=2)&&(dados.getSuma()<=6)) {
 			
-			
-			for (int i =0;i<(dados.getSuma()-jugador.cantPropiedades());i++)
+				for (int i =0;i<(dados.getSuma()-jugador.cantPropiedades());i++)
 				
-				this.tablero.retroceder(jugador);
+					this.tablero.retroceder(jugador);
 		}
-		
-		if ((dados.getSuma()>=7)&&(dados.getSuma()<=10)) {
-			int cantidad=((jugador.getDinero())%(dados.getSuma()));
-			
-			
-			for (int i =0;i<cantidad;i++)
-				
-				this.tablero.retroceder(jugador);
-			
-		}
+	}
+	
+	
+	public void retrocederConDados11o12(Jugador jugador) {
+		Dados dados=Dados.getDados();
 		
 		if ((dados.getSuma()==11)||(dados.getSuma()==12)) {
-			
 			
 			for (int i =0;i<(dados.getSuma()-2);i++)
 				
 				this.tablero.retroceder(jugador);
 		}
+	}
+		
+	public void retrocederConDadosEntre7y10(Jugador jugador) {
+		Dados dados=Dados.getDados();
+		
+		if ((dados.getSuma()>=7)&&(dados.getSuma()<=10)) {
 			
+			int cantidad=((jugador.getDinero())%(dados.getSuma()));
+			for (int i =0;i<cantidad;i++)
+				
+				this.tablero.retroceder(jugador);
 		}
 	}
+	
+	
+	@Override
+	public void ocupar(Jugador jugador) {
+		
+		this.retrocederConDadosEntre2y6(jugador);
+		
+		this.retrocederConDadosEntre7y10(jugador);
+	
+		this.retrocederConDados11o12(jugador);
+	}
+}
--- test/RetrocesoDinamicoTest.java
+++ test/RetrocesoDinamicoTest.java
@@ -196,8 +196,120 @@ public class RetrocesoDinamicoTest {
 		retroceso.ocupar(jugador);
 		
 		assertEquals(cordobaNorte, jugador.getPosicion());
+	}
+	
+	@Test
+	public void test04JugadorCaeEnRetrocesoDinamicoSacando10EnLosDadosYNoRetrocedeNingunaPosicion() {
+		Jugador jugador = new Jugador("Jugador", 100000);
+		Dados dados = Dados.getDados();
+		Tablero tablero = new Tablero();
 		
+		Salida salida=new Salida();
+		tablero.agregarCasillero(salida);
+		Quini6 quini=new Quini6();
+		tablero.agregarCasillero(quini);
+		Grupo grupo=new Grupo();
+		Terreno bsAsSur=new Terreno("BsAsSur",grupo, 20000, 2000, 3000, 3500, 5000, 5000, 8000);
+		tablero.agregarCasillero(bsAsSur);
+		Compania edesur=new Compania("Edesur", 35000,grupo, 500, 1000);
+		tablero.agregarCasillero(edesur);
+		Terreno bsAsNorte=new Terreno("BsAsNorte",grupo, 25000, 2500, 3500, 4000, 6000, 5500, 9000);
+		tablero.agregarCasillero(bsAsNorte);
+		Carcel carcel=new Carcel();
+		tablero.agregarCasillero(carcel);
+		Terreno cordobaSur=new Terreno("CordobaSur", grupo, 18000, 1000, 1500, 2500, 3000, 2000, 3000);
+		tablero.agregarCasillero(cordobaSur);
+		AvanceDinamico avance=new AvanceDinamico(tablero);
+		tablero.agregarCasillero(avance);
+		Compania subte=new Compania("Subte", 40000,grupo, 600, 1100);
+		tablero.agregarCasillero(subte);
+		Terreno cordobaNorte=new Terreno("CordobaNorte",grupo, 20000, 1300, 1800, 2900, 3500, 2200, 3500);
+		tablero.agregarCasillero(cordobaNorte);
+		ImpuestoDeLujo impuesto=new ImpuestoDeLujo();
+		tablero.agregarCasillero(impuesto);
+		Terreno santaFe=new Terreno("SantaFe",grupo, 15000, 1500, 3500, 4000, 0, 0, 0);
+		tablero.agregarCasillero(santaFe);
+		Compania aysa=new Compania("Aysa", 30000,grupo, 300, 500);
+		tablero.agregarCasillero(aysa);
+		Terreno saltaNorte=new Terreno("SaltaNorte",grupo, 23000, 2000, 3250, 3850, 5500, 4500, 7500);
+		tablero.agregarCasillero(saltaNorte);
+		Terreno saltaSur=new Terreno("SaltaSur",grupo, 23000, 2000, 3250, 3850, 5500, 4500, 7500);
+		tablero.agregarCasillero(saltaSur);
+		Policia policia=new Policia(carcel);
+		tablero.agregarCasillero(policia);
+		Compania tren=new Compania("Tren", 38000,grupo, 450, 800);
+		tablero.agregarCasillero(tren);
+		Terreno neuquen=new Terreno("Neuquen",grupo, 17000, 1800, 3800, 4800, 0, 0, 0);
+		tablero.agregarCasillero(neuquen);
+		RetrocesoDinamico retroceso=new RetrocesoDinamico(tablero);
+		tablero.agregarCasillero(retroceso);
+		Terreno tucuman=new Terreno("Tucuman",grupo, 2500, 7000, 0, 0, 0, 0, 0);
+		tablero.agregarCasillero(tucuman);
 		
+		jugador.moverA(retroceso);
+		
+		dados.setNumeros(5,5);
+		
+		retroceso.ocupar(jugador);
+		
+		assertEquals(retroceso, jugador.getPosicion());
+	}
+	
+	@Test
+	public void test05JugadorCaeEnRetrocesoDinamicoSacando9EnLosDadosYRetrocedeASaltaNorte() {
+		Jugador jugador = new Jugador("Jugador", 77000);
+		Dados dados = Dados.getDados();
+		Tablero tablero = new Tablero();
+		
+		Salida salida=new Salida();
+		tablero.agregarCasillero(salida);
+		Quini6 quini=new Quini6();
+		tablero.agregarCasillero(quini);
+		Grupo grupo=new Grupo();
+		Terreno bsAsSur=new Terreno("BsAsSur",grupo, 20000, 2000, 3000, 3500, 5000, 5000, 8000);
+		tablero.agregarCasillero(bsAsSur);
+		Compania edesur=new Compania("Edesur", 35000,grupo, 500, 1000);
+		tablero.agregarCasillero(edesur);
+		Terreno bsAsNorte=new Terreno("BsAsNorte",grupo, 25000, 2500, 3500, 4000, 6000, 5500, 9000);
+		tablero.agregarCasillero(bsAsNorte);
+		Carcel carcel=new Carcel();
+		tablero.agregarCasillero(carcel);
+		Terreno cordobaSur=new Terreno("CordobaSur", grupo, 18000, 1000, 1500, 2500, 3000, 2000, 3000);
+		tablero.agregarCasillero(cordobaSur);
+		AvanceDinamico avance=new AvanceDinamico(tablero);
+		tablero.agregarCasillero(avance);
+		Compania subte=new Compania("Subte", 40000,grupo, 600, 1100);
+		tablero.agregarCasillero(subte);
+		Terreno cordobaNorte=new Terreno("CordobaNorte",grupo, 20000, 1300, 1800, 2900, 3500, 2200, 3500);
+		tablero.agregarCasillero(cordobaNorte);
+		ImpuestoDeLujo impuesto=new ImpuestoDeLujo();
+		tablero.agregarCasillero(impuesto);
+		Terreno santaFe=new Terreno("SantaFe",grupo, 15000, 1500, 3500, 4000, 0, 0, 0);
+		tablero.agregarCasillero(santaFe);
+		Compania aysa=new Compania("Aysa", 30000,grupo, 300, 500);
+		tablero.agregarCasillero(aysa);
+		Terreno saltaNorte=new Terreno("SaltaNorte",grupo, 23000, 2000, 3250, 3850, 5500, 4500, 7500);
+		tablero.agregarCasillero(saltaNorte);
+		Terreno saltaSur=new Terreno("SaltaSur",grupo, 23000, 2000, 3250, 3850, 5500, 4500, 7500);
+		tablero.agregarCasillero(saltaSur);
+		Policia policia=new Policia(carcel);
+		tablero.agregarCasillero(policia);
+		Compania tren=new Compania("Tren", 38000,grupo, 450, 800);
+		tablero.agregarCasillero(tren);
+		Terreno neuquen=new Terreno("Neuquen",grupo, 17000, 1800, 3800, 4800, 0, 0, 0);
+		tablero.agregarCasillero(neuquen);
+		RetrocesoDinamico retroceso=new RetrocesoDinamico(tablero);
+		tablero.agregarCasillero(retroceso);
+		Terreno tucuman=new Terreno("Tucuman",grupo, 2500, 7000, 0, 0, 0, 0, 0);
+		tablero.agregarCasillero(tucuman);
+		
+		jugador.moverA(retroceso);
+		
+		dados.setNumeros(5,4);
+		
+		retroceso.ocupar(jugador);
+		
+		assertEquals(saltaNorte, jugador.getPosicion());
 	}
 
 }
