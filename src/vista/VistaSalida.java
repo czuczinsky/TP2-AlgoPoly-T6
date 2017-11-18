e96a246da0e584701678d7bcc442487b9bc81012
acevedoanalia <analiaacevedo020@gmail.com>
2017-11-18 19:48:06 -0300

    Se agregan imagenes a las vistas

--- src/vista/VistaSalida.java
+++ src/vista/VistaSalida.java
@@ -1,7 +1,11 @@
 package vista;
 
+import javafx.geometry.VPos;
 import javafx.scene.canvas.Canvas;
+import javafx.scene.canvas.GraphicsContext;
+import javafx.scene.image.Image;
 import javafx.scene.paint.Color;
+import javafx.scene.text.TextAlignment;
 import modelo.Casillero;
 
 public class VistaSalida extends VistaRectangulo implements Dibujable {
@@ -16,7 +20,13 @@ public class VistaSalida extends VistaRectangulo implements Dibujable {
 	}
 
 	public void dibujar() {
-		this.pintar(Color.GREY);
-		this.etiquetar("SALIDA");
-	}
+		
+		this.pintar(Color.GAINSBORO);
+		Image image = new Image("file:src/vista/imagenes/salida.png");
+		GraphicsContext gc = canvas.getGraphicsContext2D();
+		gc.drawImage(image, x + (ancho - image.getWidth()) / 2, y + (alto - image.getHeight()) / 2);
+		canvas.getGraphicsContext2D().setFill(Color.WHITE);
+		gc.setTextAlign(TextAlignment.CENTER);
+		gc.setTextBaseline(VPos.BOTTOM);
+		canvas.getGraphicsContext2D().fillText("", x + ancho / 2, y + alto);	}
 }
