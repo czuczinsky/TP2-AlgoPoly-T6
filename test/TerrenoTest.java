3f95e3bab9c430a096c026d9e2e890fd230a1c00
acevedoanalia <analiaacevedo020@gmail.com>
2017-11-17 21:09:28 -0300

    Test completos hasta el item 6 de los requerimientos para la 2da entrega

--- test/TerrenoTest.java
+++ test/TerrenoTest.java
@@ -324,5 +324,110 @@ public class TerrenoTest {
 		neuquen.venderA(jugador1);
 		Assert.assertEquals(75000 , jugador1.getDinero());
 	}
+	
+	@Test
+	public void test24JugadorQueEsPropietarioDeBsAsSurYNorteSiConstruyeUnaCasaEnElSurSuDineroDeberiaDecrementarseEn5000() {
+		Grupo buenosAires = new Grupo();
+		Terreno bsAsSur = new Terreno("Buenos Aires Sur", buenosAires,
+				20000 , 2000 , 3000 , 3500 , 5000 , 5000 , 8000);
+		
+		Terreno bsAsNorte=new Terreno("Buenos Aires Norte", buenosAires, 
+				25000 , 2500 , 3500 , 4000 , 6000, 5500 , 9000);
+				
+		Jugador jugador1 = new Jugador("Jugador 1",100000);
+		bsAsSur.venderA(jugador1);
+		bsAsNorte.venderA(jugador1);
+		Assert.assertEquals(55000 , jugador1.getDinero());
+		bsAsSur.construirCasa();
+	
+		Assert.assertEquals( 55000-5000, jugador1.getDinero());
+	}
+	@Test
+	public void test25JugadorQueEsPropietarioDeBsAsSurYNorteConUnaCasaEnCadaUnaSiContrincanteCaeEnElSurSuDineroSeDecrementaEn3Mil() {
+		Grupo buenosAires = new Grupo();
+		Dados dados=new Dados();
+		Terreno bsAsSur = new Terreno("Buenos Aires Sur", buenosAires,
+				20000 , 2000 , 3000 , 3500 , 5000 , 5000 , 8000);
+		
+		Terreno bsAsNorte=new Terreno("Buenos Aires Norte", buenosAires, 
+				25000 , 2500 , 3500 , 4000 , 6000, 5500 , 9000);
+				
+		Jugador juan = new Jugador("Juan",100000);
+		bsAsSur.venderA(juan);
+		bsAsNorte.venderA(juan);
+		bsAsSur.construirCasa();
+		bsAsNorte.construirCasa();
+		Jugador carlos=new Jugador ("Carlos",13000);
+		
+		bsAsSur.ocupar(carlos, dados);
+	
+		Assert.assertEquals( 13000-3000, carlos.getDinero());
+	}
+	
+	@Test
+	public void test26JugadorQueEsPropietarioDeBsAsSurYNorteConUnaCasaEnCadaUnaSiContrincanteCaeEnElSurSuDineroSeDecrementaEn3Mil() {
+		Grupo buenosAires = new Grupo();
+		Dados dados=new Dados();
+		Terreno bsAsSur = new Terreno("Buenos Aires Sur", buenosAires,
+				20000 , 2000 , 3000 , 3500 , 5000 , 5000 , 8000);
+		
+		Terreno bsAsNorte=new Terreno("Buenos Aires Norte", buenosAires, 
+				25000 , 2500 , 3500 , 4000 , 6000, 5500 , 9000);
+				
+		Jugador maria = new Jugador("Maria",100000);
+		bsAsSur.venderA(maria);
+		bsAsNorte.venderA(maria);
+		bsAsSur.construirCasa();
+		bsAsSur.construirCasa();
+		bsAsNorte.construirCasa();
+		Jugador laura=new Jugador ("Laura",13000);
+		
+		bsAsNorte.ocupar(laura, dados);
+	
+		Assert.assertEquals( 13000-3500, laura.getDinero());
+	}
+	@Test
+	public void test27JugadorQueEsPropietarioDeBsAsSurYNorteYTiene3CasasCuandoConstruyeUnHotelSuDineroNoDeberiaDecrementarse() {
+		Grupo buenosAires = new Grupo();
+		Terreno bsAsSur = new Terreno("Buenos Aires Sur", buenosAires,
+				20000 , 2000 , 3000 , 3500 , 5000 , 5000 , 8000);
+		
+		Terreno bsAsNorte=new Terreno("Buenos Aires Norte", buenosAires, 
+				25000 , 2500 , 3500 , 4000 , 6000, 5500 , 9000);
+				
+		Jugador lucas= new Jugador("Lucas",100000);
+		bsAsSur.venderA(lucas);
+		bsAsNorte.venderA(lucas);
+		bsAsSur.construirCasa();
+		bsAsSur.construirCasa();
+		bsAsNorte.construirCasa();
+		Assert.assertEquals( 100000-20000-25000-5000-5000-5500, lucas.getDinero());
+		bsAsNorte.construirHotel();
+		Assert.assertEquals( 100000-20000-25000-5000-5000-5500, lucas.getDinero());
+
+	}
+	
+	@Test
+	public void test28JugadorQueEsPropietarioDeBsAsSurYNorteYTiene4CasasCuandoConstruyeUnHotelEnElSurSuDineroDeberiaDecrementarse8Mil() {
+		Grupo buenosAires = new Grupo();
+		Terreno bsAsSur = new Terreno("Buenos Aires Sur", buenosAires,
+				20000 , 2000 , 3000 , 3500 , 5000 , 5000 , 8000);
+		
+		Terreno bsAsNorte=new Terreno("Buenos Aires Norte", buenosAires, 
+				25000 , 2500 , 3500 , 4000 , 6000, 5500 , 9000);
+				
+		Jugador pablo= new Jugador("Pablo",100000);
+		bsAsSur.venderA(pablo);
+		bsAsNorte.venderA(pablo);
+		bsAsSur.construirCasa();
+		bsAsSur.construirCasa();
+		bsAsNorte.construirCasa();
+		bsAsNorte.construirCasa();
+		Assert.assertEquals( 100000-20000-25000-5000-5000-5500-5500, pablo.getDinero());
+		bsAsSur.construirHotel();
+		Assert.assertEquals(34000 -8000, pablo.getDinero());
+
+	}
+
 	// TODO tests de casos en que ya no se puede edificar
 }
