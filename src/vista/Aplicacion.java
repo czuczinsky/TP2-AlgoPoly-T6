c3b6ef4a4df574d0d7189e4e2d8674c53cfd33cb
hhuzan <hhuzan@192.168.1.102>
2017-11-28 22:16:34 -0300

    limieza Aplicacion.java

--- src/vista/Aplicacion.java
+++ src/vista/Aplicacion.java
@@ -1,15 +1,9 @@
 package vista;
 
-import java.net.URL;
-
 import javafx.application.Application;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
-//import vista.eventos.AplicacionOnKeyPressEventHandler;
 import modelo.AlgoPoly;
-import javafx.util.Duration;  
-import javafx.scene.media.Media;  
-import javafx.scene.media.MediaPlayer;
 
 
 public class Aplicacion extends Application  {
@@ -27,17 +21,7 @@ public class Aplicacion extends Application  {
 
         ContenedorPrincipal contenedorPrincipal = new ContenedorPrincipal(algoPoly, stage);
         Scene escenaJuego = new Scene(contenedorPrincipal, 1160, 670);
-
-
- //        AplicacionOnKeyPressEventHandler AplicacionOnKeyPressEventHandler = new AplicacionOnKeyPressEventHandler(stage, contenedorPrincipal.getBarraDeMenu());
- //       escenaJuego.setOnKeyPressed(AplicacionOnKeyPressEventHandler);
-
-
-        // add handler to this:
-        // stage.setOnCloseRequest()
-
         stage.setScene(escenaJuego);
-//        stage.setFullScreen(true);
 
         stage.show();
 
